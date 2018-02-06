cbec51c0f98442af9f2b8beea97f81fb00ee0c28
Mahmoud Zidan <mahmoud.zidan95@eng-st.cu.edu.eg>
2018-02-06 16:06:49 +0200

    Update and rename templete_trainer.py to template_trainer.py

--- trainers/template_trainer.py
+++ trainers/template_trainer.py
@@ -3,15 +3,15 @@ from tqdm import tqdm
 import numpy as np
 
 
-class TempleteTrainer(BaseTrain):
+class TemplateTrainer(BaseTrain):
     def __init__(self, sess, model, data, config, logger):
-        super(TempleteTrainer, self).__init__(sess, model, data, config, logger)
+        super(TemplateTrainer, self).__init__(sess, model, data, config, logger)
 
     def train_epoch(self):
         """
        implement the logic of epoch:
-       -loop ever the number of iteration in the config and call teh train step
-       -add any summaries you want using the sammary
+       -loop on the number of iterations in the config and call the train step
+       -add any summaries you want using the summary
         """
         pass
 
