eae68781cc53763dd13896437d547fb63aef7f2c
Moemen AbdelRazek <runmoemen@gmail.com>
2018-03-15 01:26:59 +0200

    add test function in the trainer and override function train

--- trainers/cifar_trainer.py
+++ trainers/cifar_trainer.py
@@ -30,11 +30,21 @@ class CifarTrainer(BaseTrain):
         self.model.load(self.sess)
 
         # Summarizer
-        self.summarizer = DefinedSummarizer(self.sess, self.config.summary_dir,
-                                            ['train/loss_per_epoch', 'train/acc_per_epoch'])
+        self.summarizer = logger
 
         self.x, self.y, self.is_training = tf.get_collection('inputs')
-        self.train_step, self.loss_node, self.acc_node = tf.get_collection('train')
+        self.train_op, self.loss_node, self.acc_node = tf.get_collection('train')
+    
+    def train(self):
+        """
+        This is the main loop of training
+        Looping on the epochs
+        :return:
+        """
+        for cur_epoch in range(self.model.cur_epoch_tensor.eval(self.sess), self.config.num_epochs + 1, 1):
+            self.train_epoch(cur_epoch)
+            self.sess.run(self.model.increment_cur_epoch_tensor)
+            self.test(cur_epoch)
 
     def train_epoch(self, epoch=None):
         """
@@ -68,6 +78,10 @@ class CifarTrainer(BaseTrain):
         self.summarizer.summarize(self.model.global_step_tensor.eval(self.sess), summaries_dict)
 
         self.model.save(self.sess)
+        
+        print("""
+Epoch-{}  loss:{:.4f} -- acc:{:.4f}
+        """.format(epoch, loss_per_epoch.val, acc_per_epoch.val))
 
         tt.close()
 
@@ -77,6 +91,37 @@ class CifarTrainer(BaseTrain):
         also get the loss & acc of that minibatch.
         :return: (loss, acc) tuple of some metrics to be used in summaries
         """
-        _, loss, acc = self.sess.run([self.train_step, self.loss_node, self.acc_node],
+        _, loss, acc = self.sess.run([self.train_op, self.loss_node, self.acc_node],
                                      feed_dict={self.is_training: True})
         return loss, acc
+    
+    def test(self, epoch):
+        # initialize dataset
+        self.data_loader.initialize(self.sess, is_train=False)
+
+        # initialize tqdm
+        tt = tqdm(range(self.data_loader.num_iterations_test), total=self.data_loader.num_iterations_test,
+                  desc="Val-{}-".format(epoch))
+
+        loss_per_epoch = AverageMeter()
+        acc_per_epoch = AverageMeter()
+
+        # Iterate over batches
+        for cur_it in tt:
+            # One Train step on the current batch
+            loss, acc = self.sess.run([self.loss_node, self.acc_node],
+                                     feed_dict={self.is_training: False})
+            # update metrics returned from train_step func
+            loss_per_epoch.update(loss)
+            acc_per_epoch.update(acc)
+
+        # summarize
+        summaries_dict = {'test/loss_per_epoch': loss_per_epoch.val,
+                          'test/acc_per_epoch': acc_per_epoch.val}
+        self.summarizer.summarize(self.model.global_step_tensor.eval(self.sess), summaries_dict)
+        
+        print("""
+Val-{}  loss:{:.4f} -- acc:{:.4f}
+        """.format(epoch, loss_per_epoch.val, acc_per_epoch.val))
+
+        tt.close()
